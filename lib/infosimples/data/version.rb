a5c424403db9dd45818ee3c7bd3e5392c9542735
Rafael Barbolo <rafael.barbolo@infosimples.com.br>
2018-10-09 09:39:15 -0300

    Bump to version 1.9.4

--- lib/infosimples/data/version.rb
+++ lib/infosimples/data/version.rb
@@ -1,6 +1,6 @@
 module Infosimples
   module Data
-    VERSION = "1.9.3" # https://data.infosimples.com/docs/changelog
+    VERSION = "1.9.4" # https://data.infosimples.com/docs/changelog
     USER_AGENT = "Infosimples::Data/Ruby v#{VERSION}"
   end
 end
